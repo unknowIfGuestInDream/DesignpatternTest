c40fc6992a9f6623cf55af370d15cd0bb70202da
唐亮 <tang97155@163.com>
2021-02-03 19:52:06 +0800

    feat(单例的深克隆):

--- src/原型/拷贝单例类/Test2.java
+++ src/原型/拷贝单例类/Test2.java
@@ -6,11 +6,17 @@ package 原型.拷贝单例类;
  * @since: 1.0
  */
 public class Test2 {
+    public static DbUtil oracle = DbUtil.getInstance();
+    public static DbUtil mysql = (DbUtil) oracle.clone();
+    public static DbUtil sqlserver = (DbUtil) oracle.clone();
+
     public static void main(String[] args) {
-        System.out.println(DbUtil.getInstance());
-        System.out.println(DbUtil.getInstance());
+        System.out.println(oracle);
+        System.out.println(mysql);
+
         DbUtil.getInstance().setDriver("aaa");
-        DbUtil.getInstance().init();
+        System.out.println(oracle);
+        System.out.println(sqlserver);
         System.out.println(DbUtil.getInstance().clone());
         System.out.println(DbUtil.getInstance().clone());
     }
