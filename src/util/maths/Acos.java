843f1b04328e6e80ac596ffbf5f33af9e9884bf2
唐亮 <tang97155@163.com>
2022-10-03 23:28:13 +0800

    feat: Math.acos

---
+++ src/util/maths/Acos.java
@@ -0,0 +1,14 @@
+package util.maths;
+
+/**
+ * @author: 唐 亮
+ * @date: 2022/7/30 22:34
+ * @since: 1.0
+ */
+public class Acos {
+
+    public static void main(String[] args) {
+        System.out.println(Math.acos(1.0));
+        System.out.println(Math.acos(0.5));
+    }
+}
